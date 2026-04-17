fa5b3c3bbf2ee6e2eaeb32135954100a34f1828c
Kasi <simply.kasi@gmail.com>
2026-04-17 20:50:56 +0530

    Simulation Added

--- Blink/main.c
+++ Blink/main.c
@@ -2,14 +2,14 @@
 #include<avr/io.h>
 #include<util/delay.h>
 
-int main(void)
+int main()
 {
-DDRB|=(1<<DDB5);
-while(1)
-{
-PORTB|=(1<<PB5);
-_delay_ms(1000);
-PORTB&=~(1<<PB5);
-_delay_ms(1000);
+DDRB |= (1<<DDB5); // Configured Pin 5 as output
+while(1){
+PORTB |=(1<<PB5); // Pin 5 = High
+_delay_ms(300); //Delay of 1s
+PORTB &=~(1<<PB5); // Pin 5 = Low
+_delay_ms(300);
 }
+
 }
